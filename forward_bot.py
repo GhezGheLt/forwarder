d7e092d91c41b9c197c235c7783aa89fd6a61840
GhezGheLt <109137794+GhezGheLt@users.noreply.github.com>
2025-05-12 00:34:53 +0330

    forward_bot.py

--- forward_bot.py
+++ forward_bot.py
@@ -2,6 +2,7 @@ from keep_alive import keep_alive
 keep_alive()
 
 from pyrogram import Client, filters
+import traceback
 
 api_id = 8891803
 api_hash = "5908205e3e6f563d76e6bd8f87723c1d"
@@ -9,20 +10,15 @@ bot_token = "7399010656:AAF6hrFA15MyBoDEfuI2qN_OZgv5fcbwlLA"
 
 app = Client("forward_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-# آی‌دی کانال منبع
-source_channel = -1002650282186
-
-# آیدی عددی کانال مقصد رو بعد از اولین بار به دست میاریم
-destination_chat_id = None
+source_channel = -1002650282186  # آیدی عددی کانال منبع
+destination_chat_id = None       # برای ذخیره آیدی کانال مقصد
 
 @app.on_message(filters.chat(source_channel))
 async def forward_and_edit_caption(client, message):
     global destination_chat_id
-
     try:
-        # اگر آی‌دی مقصد هنوز مشخص نشده، برای بار اول از روی لینک دعوت آن را دریافت کن
         if destination_chat_id is None:
-            chat = await client.join_chat("https://t.me/+UUbibYEDvhM1MTBi")
+            chat = await client.get_chat("https://t.me/+UUbibYEDvhM1MTBi")
             destination_chat_id = chat.id
             print("Destination chat ID:", destination_chat_id)
 
@@ -48,5 +44,6 @@ async def forward_and_edit_caption(client, message):
 
     except Exception as e:
         print("Error forwarding and editing caption:", e)
+        traceback.print_exc()
 
 app.run()
