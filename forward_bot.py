1459fc4e6fb299cafb6012570597be160ad14de1
GhezGheLt <109137794+GhezGheLt@users.noreply.github.com>
2025-05-12 17:13:14 +0330

    forward_bot.py

--- forward_bot.py
+++ forward_bot.py
@@ -1,24 +1,23 @@
 from pyrogram import Client, filters
+import os
+from dotenv import load_dotenv
 
-api_id = 8891803
-api_hash = "5908205e3e6f563d76e6bd8f87723c1d"
-bot_token = "7399010656:AAF6hrFA15MyBoDEfuI2qN_OZgv5fcbwlLA"
+# بارگذاری متغیرهای محیطی
+load_dotenv()
+
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
+bot_token = os.getenv("BOT_TOKEN")
 
 app = Client("forward_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-source_channel = -1002650282186
-destination_channel = -1002293369181  # آیدی عددی کانال مقصد
+# آیدی عددی کانال‌ها
+SOURCE_CHANNEL = int(os.getenv("SOURCE_CHANNEL"))
+DEST_CHANNEL = int(os.getenv("DEST_CHANNEL"))
 
-@app.on_message(filters.chat(source_channel))
+@app.on_message(filters.chat(SOURCE_CHANNEL))
 async def forward_and_edit_caption(client, message):
     try:
-        # تلاش برای resolve کردن آیدی عددی کانال
-        try:
-            await client.get_chat(destination_channel)
-        except Exception as e:
-            print("get_chat error:", e)
-            return
-
         original_caption = message.caption or ""
         first_line = original_caption.split('\n')[0] if original_caption else ""
 
@@ -29,17 +28,17 @@ async def forward_and_edit_caption(client, message):
         )
 
         if message.photo:
-            await client.send_photo(destination_channel, photo=message.photo.file_id, caption=new_caption, parse_mode="Markdown")
+            await client.send_photo(DEST_CHANNEL, photo=message.photo.file_id, caption=new_caption, parse_mode="Markdown")
         elif message.video:
-            await client.send_video(destination_channel, video=message.video.file_id, caption=new_caption, parse_mode="Markdown")
+            await client.send_video(DEST_CHANNEL, video=message.video.file_id, caption=new_caption, parse_mode="Markdown")
         elif message.document:
-            await client.send_document(destination_channel, document=message.document.file_id, caption=new_caption, parse_mode="Markdown")
+            await client.send_document(DEST_CHANNEL, document=message.document.file_id, caption=new_caption, parse_mode="Markdown")
         elif message.text:
-            await client.send_message(destination_channel, text=new_caption, parse_mode="Markdown")
+            await client.send_message(DEST_CHANNEL, text=new_caption, parse_mode="Markdown")
         else:
             print("Unsupported message type.")
 
     except Exception as e:
-        print("Error forwarding and editing caption:", e)
+        print("Error:", e)
 
 app.run()
